e56bbdda6b08b4ab5a28b232360228db4dd63295
khushpreet kaur <khushpreetkaur98155@gmail.com>
2026-02-22 12:03:36 -0500

    updated homepage text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello World</div>;
+  return <div>Welcome to my COMP2156 Assignment</div>;
+
+  <h2>Khushpreet kaur - Student ID - 101574996</h2>
 }
 
 export default App;
